6044b5eeef55b3866feeea33640a84386f6f7aa2
nashiora <22553694+nashiora@users.noreply.github.com>
2023-05-30 15:42:51 -0400

    Add preamble files

--- src/libc_s/stdio.h
+++ src/libc_s/stdio.h
@@ -4,27 +4,37 @@
 #include <errno.h>
 #include <stdio.h>
 
-typedef enum fopen_err {
-  FOPEN_NO_ACCESS = EACCES,
-  FOPEN_BAD_FILE_DESCRIPTOR = EBADF,
-  FOPEN_EXISTS = EEXIST,
-  FOPEN_TOO_BIG = EFBIG,
-  FOPEN_INVALID_ARGUMENT = EINVAL,
-  FOPEN_TOO_MANY_FILES_DESCRIPTORS = EMFILE,
-  FOPEN_NAME_TOO_LONG = ENAMETOOLONG,
-  FOPEN_TOO_MANY_FILES_OPEN = ENFILE,
-  FOPEN_DOES_NOT_EXIST = ENOENT,
-  FOPEN_READ_ONLY = EROFS,
-  FOPEN_CROSS_DEVICE_LINK = EXDEV,
-  FOPEN_TEXT_FILE_BUSY = ETXTBSY,
+typedef enum fopen_err
+{
+    FOPEN_NO_ACCESS = EACCES,
+    FOPEN_BAD_FILE_DESCRIPTOR = EBADF,
+    FOPEN_EXISTS = EEXIST,
+    FOPEN_TOO_BIG = EFBIG,
+    FOPEN_INVALID_ARGUMENT = EINVAL,
+    FOPEN_TOO_MANY_FILES_DESCRIPTORS = EMFILE,
+    FOPEN_NAME_TOO_LONG = ENAMETOOLONG,
+    FOPEN_TOO_MANY_FILES_OPEN = ENFILE,
+    FOPEN_DOES_NOT_EXIST = ENOENT,
+    FOPEN_READ_ONLY = EROFS,
+    FOPEN_CROSS_DEVICE_LINK = EXDEV,
+    FOPEN_TEXT_FILE_BUSY = ETXTBSY,
 } fopen_err;
+
+typedef enum fclose_err
+{
+    FCLOSE_INVALID_ARGUMENT = EINVAL,
+} fclose_err;
 
 typedef struct { FILE* value; fopen_err error; } fopen_result;
 fopen_result fopen_wrapper(const char* file, const char* mode);
 
+fclose_err fclose_wrapper(FILE* stream);
+
+
 #if defined(LIBC_S_STDIO_IMPLEMENTATION) || defined(LIBC_S_IMPLEMENTATION)
 
 fopen_result fopen_wrapper(const char* file, const char* mode)
+
 {
   fopen_result result = { 0 };
   errno = 0;
@@ -33,9 +43,19 @@ fopen_result fopen_wrapper(const char* file, const char* mode)
   else result.value = libcResult;
   return result;
 }
+fclose_err fclose_wrapper(FILE* stream)
+
+{
+  fclose_err result = 0;
+  errno = 0;
+  fclose(stream);
+  if (errno != 0) result = errno;
+  return result;
+}
 
 #endif // defined(LIBC_S_STDIO_IMPLEMENTATION) || defined(LIBC_S_IMPLEMENTATION)
 
 #define fopen(file, mode) fopen_wrapper(file, mode)
+#define fclose(stream) fclose_wrapper(stream)
 
 #endif // STDIO
--- src/main.c
+++ src/main.c
@@ -1,4 +1,4 @@
-#define USE_LIBC_S
+//#define USE_LIBC_S
 
 #include <assert.h>
 #include <ctype.h>
@@ -8,31 +8,10 @@
 
 #ifdef USE_LIBC_S
 #define LIBC_S_IMPLEMENTATION
-#include "libc_s/stdio.h"
+#include "libc_s/sstdio.h"
 #else
 #include <stdio.h>
 #endif
-
-#define STDIO_PREAMBLE "typedef enum fopen_err {\n" \
-"  FOPEN_NO_ACCESS = EACCES,\n" \
-"  FOPEN_BAD_FILE_DESCRIPTOR = EBADF,\n" \
-"  FOPEN_EXISTS = EEXIST,\n" \
-"  FOPEN_TOO_BIG = EFBIG,\n" \
-"  FOPEN_INVALID_ARGUMENT = EINVAL,\n" \
-"  FOPEN_TOO_MANY_FILES_DESCRIPTORS = EMFILE,\n" \
-"  FOPEN_NAME_TOO_LONG = ENAMETOOLONG,\n" \
-"  FOPEN_TOO_MANY_FILES_OPEN = ENFILE,\n" \
-"  FOPEN_DOES_NOT_EXIST = ENOENT,\n" \
-"  FOPEN_READ_ONLY = EROFS,\n" \
-"  FOPEN_CROSS_DEVICE_LINK = EXDEV,\n" \
-"  FOPEN_TEXT_FILE_BUSY = ETXTBSY,\n" \
-"} fopen_err;\n"
-
-typedef struct libc_header_and_preamble
-{
-    const char* libcHeaderName;
-    const char* wrapperPreamble;
-} libc_header_and_preamble;
 
 typedef enum libc_err_kind
 {
@@ -50,15 +29,17 @@ typedef struct libc_data
     const char* libcArguments;
 } libc_data;
 
-const libc_header_and_preamble libcHeaderFiles[] = {
-    { "stdio.h", STDIO_PREAMBLE },
-    { 0 },
+const char* libcHeaderFiles[] = {
+    "stdio.h",
+    0,
 };
-#define LIBC_HEADER_COUNT (sizeof(libcHeaderFiles) / sizeof(libc_header_and_preamble) - 1)
+#define LIBC_HEADER_COUNT (sizeof(libcHeaderFiles) / sizeof(char*) - 1)
 
 const libc_data libcData[] = {
     { "stdio.h", "fopen", "FILE*", "fopen_err", LIBC_ERR_ERRNO,
       "const char* file, const char* mode", "file, mode" },
+    { "stdio.h", "fclose", NULL, "fclose_err", LIBC_ERR_ERRNO,
+      "FILE* stream", "stream" },
     { 0 },
 };
 #define LIBC_DATA_COUNT (sizeof(libcData) / sizeof(libc_data) - 1)
@@ -66,13 +47,54 @@ const libc_data libcData[] = {
 static void write_wrapper_prototype(FILE* stream, libc_data data);
 static void write_wrapper_body(FILE* stream, libc_data data);
 
+static char* read_preamble_file(const char* preambleDir, const char* headerFileName)
+{
+    char preambleFileNameBuffer[256];
+    strcpy(preambleFileNameBuffer, preambleDir);
+
+    size_t i = 0;
+    for (; i < 256; i++)
+    {
+        if (preambleFileNameBuffer[i] == 0)
+        {
+            preambleFileNameBuffer[i++] = '/';
+            break;
+        }
+    }
+
+    strcpy(preambleFileNameBuffer + i, headerFileName);
+
+#ifdef USE_LIBC_S
+    fopen_result preambleFileResult = fopen(preambleFileNameBuffer, "r");
+    FILE* preambleFile = preambleFileResult.value;
+#else
+    FILE* preambleFile = fopen(preambleFileNameBuffer, "r");
+#endif
+    if (preambleFile == NULL)
+    {
+        fprintf(stderr, "Failed to open file %s for reading: %s", preambleFileNameBuffer, strerror(errno));
+        exit(0);
+    }
+
+    fseek(preambleFile, 0, SEEK_END);
+    size_t fileLength = (size_t)ftell(preambleFile);
+    fseek(preambleFile, 0, SEEK_SET);
+
+    char* buffer = calloc(sizeof(char), fileLength + 1);
+    fread(buffer, fileLength, 1, preambleFile);
+
+    return buffer;
+}
+
 int main(int argc, char** argv)
 {
-    assert(libcHeaderFiles[LIBC_HEADER_COUNT].libcHeaderName == NULL);
+    assert(libcHeaderFiles[LIBC_HEADER_COUNT] == NULL);
     assert(libcData[LIBC_DATA_COUNT].libcFunctionName == NULL);
     
-    assert(argc >= 2);
-    const char* outDir = argv[1];
+    const char* preambleDir = "./src/libc_s/preamble";
+    size_t preambleDirLen = strlen(preambleDir);
+
+    const char* outDir = "./src/libc_s";
     size_t outDirLen = strlen(outDir);
 
     char fileNameBuffer[256];
@@ -81,8 +103,8 @@ int main(int argc, char** argv)
 
     for (int j = 0; j < LIBC_HEADER_COUNT; j++)
     {
-        const char* headerFileName = libcHeaderFiles[j].libcHeaderName;
-        size_t headerFileNameLength= strlen(headerFileName);
+        const char* headerFileName = libcHeaderFiles[j];
+        size_t headerFileNameLength = strlen(headerFileName);
         
         memset(fileNameBuffer + outDirLen + 1, 0, 256 - outDirLen - 1);
         memcpy(fileNameBuffer + outDirLen + 1, headerFileName, headerFileNameLength);
@@ -99,8 +121,6 @@ int main(int argc, char** argv)
         }
 #endif
         
-        const char* headerPreamble = libcHeaderFiles[j].wrapperPreamble;
-
         char headerNameBuffer[256];
         for (size_t k = 0; k < headerFileNameLength; k++)
         {
@@ -112,8 +132,11 @@ int main(int argc, char** argv)
             headerNameBuffer[k] = toupper(headerFileName[k]);
         }
         fprintf(headerFile, "#ifndef LIBC_S_%s_H\n#define LIBC_S_%s_H\n\n", headerNameBuffer, headerNameBuffer);
-        fprintf(headerFile, "#include <errno.h>\n#include <%s>\n\n", headerFileName);
+        
+        char* headerPreamble = read_preamble_file(preambleDir, headerFileName);
         fprintf(headerFile, "%s\n", headerPreamble);
+        fflush(headerFile);
+        free(headerPreamble);
 
         for (int i = 0; i < LIBC_DATA_COUNT; i++)
         {
@@ -129,7 +152,7 @@ int main(int argc, char** argv)
             }
 
             write_wrapper_prototype(headerFile, data);
-            fprintf(headerFile, ";\n");
+            fprintf(headerFile, ";\n\n");
         }
 
         fprintf(headerFile, "\n#if defined(LIBC_S_%s_IMPLEMENTATION) || defined(LIBC_S_IMPLEMENTATION)\n\n", headerNameBuffer);
@@ -140,7 +163,7 @@ int main(int argc, char** argv)
                 continue;
                 
             write_wrapper_prototype(headerFile, data);
-            fprintf(headerFile, "\n");
+            fprintf(headerFile, "\n\n");
             write_wrapper_body(headerFile, data);
         }
         fprintf(headerFile, "\n#endif // defined(LIBC_S_%s_IMPLEMENTATION) || defined(LIBC_S_IMPLEMENTATION)\n\n", headerNameBuffer);
@@ -167,7 +190,7 @@ static void write_wrapper_prototype(FILE* stream, libc_data data)
 
     if (isVoid)
     {
-        fprintf(stream, "%s %s_wrapper(%s)", data.libcReturnType, data.libcFunctionName, data.wrapperParams);
+        fprintf(stream, "%s %s_wrapper(%s)", data.wrapperReturnType, data.libcFunctionName, data.wrapperParams);
     }
     else
     {
